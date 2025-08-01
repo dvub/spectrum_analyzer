664704a54daf6cc8886b65468b69a74737bd6f47
dvub <dvubdevs@gmail.com>
2025-07-31 21:57:29 -0700

    remove unused param

--- src/params.rs
+++ src/params.rs
@@ -5,13 +5,6 @@ use nih_plug_webview::WebViewState;
 
 #[derive(Params)]
 pub struct SpectrumAnalyzerParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
-    #[id = "gain"]
-    pub gain: FloatParam,
-
     #[persist = "webview_state"]
     pub state: Arc<WebViewState>,
 }
@@ -20,30 +13,6 @@ impl Default for SpectrumAnalyzerParams {
     fn default() -> Self {
         Self {
             state: Arc::new(WebViewState::new(600.0, 600.0)),
-
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
-            gain: FloatParam::new(
-                "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
-                },
-            )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
-            .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
-            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
         }
     }
 }
