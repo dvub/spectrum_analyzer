6b7b3f01f036731c562ad4e3b7f7f363a8596c94
dvub <dvubdevs@gmail.com>
2025-08-02 02:14:12 -0700

    bring monitor closer to fundsp implementation

--- src/editor/monitor.rs
+++ src/editor/monitor.rs
@@ -1,7 +1,15 @@
+// code ported from FunDSP:
+// https://github.com/SamiPerttu/fundsp/blob/a4f126bcbb5c6b93c4cd65662035655913e1e830/src/dynamics.rs#L343
+// thank you very much samip for this
+
+const DEFAULT_FPS: f32 = 30.0;
+
 #[derive(Clone, Copy)]
+#[allow(dead_code)]
 pub enum Meter {
-    Peak,
-    RMS,
+    Sample,
+    Peak(f32),
+    Rms(f32),
 }
 
 #[derive(Clone)]
@@ -9,38 +17,49 @@ pub struct Monitor {
     meter: Meter,
     state: f32,
     smoothing: f32,
-    timescale: f32,
 }
 
 impl Monitor {
-    pub fn new(meter: Meter, timescale: f32, sample_rate: f32) -> Self {
-        let smoothing = (0.5f32).powf(1.0 / (timescale * sample_rate));
-
-        Monitor {
+    /// Create a new Monitor based on the given mode.
+    pub fn new(meter: Meter) -> Self {
+        let mut monitor = Self {
             meter,
             state: 0.0,
-            smoothing,
-            timescale,
-        }
+            smoothing: 0.0,
+        };
+
+        monitor.set_frame_rate(DEFAULT_FPS);
+        monitor
     }
 
-    pub fn set_sample_rate(&mut self, sample_rate: f32) {
-        self.smoothing = (0.5f32).powf(1.0 / (self.timescale * sample_rate));
+    /// Set the frame rate at which the meter is updated.
+    pub fn set_frame_rate(&mut self, frame_rate: f32) {
+        let timescale = match self.meter {
+            Meter::Sample => {
+                return;
+            }
+            Meter::Peak(timescale) => timescale,
+            Meter::Rms(timescale) => timescale,
+        };
+        self.smoothing = 0.5f32.powf(1.0 / (timescale * frame_rate));
     }
 
+    /// Process an input value.
     pub fn tick(&mut self, value: f32) {
         match self.meter {
-            Meter::Peak => self.state = f32::max(self.state * self.smoothing, f32::abs(value)),
-            Meter::RMS => {
-                self.state = self.state * self.smoothing + (value.powi(2)) * (1.0 - self.smoothing);
+            Meter::Sample => self.state = value,
+            Meter::Peak(_) => self.state = f32::max(self.state * self.smoothing, value.abs()),
+            Meter::Rms(_) => {
+                self.state = self.state * self.smoothing + value.powi(2) * (1.0 - self.smoothing)
             }
         }
     }
 
     pub fn level(&self) -> f32 {
         match self.meter {
-            Meter::Peak => self.state,
-            Meter::RMS => self.state.sqrt(),
+            Meter::Sample => self.state,
+            Meter::Peak(_) => self.state,
+            Meter::Rms(_) => self.state.sqrt(),
         }
     }
 }
