4bb012ec93c1e4f92fed5a5743b704e9bb9187d8
dvub <dvubdevs@gmail.com>
2025-08-04 18:40:18 -0700

    add todo

--- src/dsp.rs
+++ src/dsp.rs
@@ -3,6 +3,7 @@ use fundsp::hacker32::*;
 
 pub fn build_graph(tx: Sender<f32>) -> Box<dyn AudioUnit> {
     let watcher = join::<U2>()
+    // TODO: use fundsp::snoop
         >> map(move |i| {
             let _ = tx.try_send(i[0]);
             i[0]
